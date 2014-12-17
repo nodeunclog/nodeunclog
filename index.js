bcc4f2dd43c607cd5db8c05112933c99b12136e2
nodeunclog <nodeunclog@gmail.com>
2014-12-17 15:03:35 +0530

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ if (0 || process.env.skipunclog) {
     module.exports = require('./skip');
     return;
 }
-if (1 || process.env.blankunclog) {
+if (0 || process.env.blankunclog) {
     console.log('Silence is golden.');
     module.exports = require('./blank');
     return;
