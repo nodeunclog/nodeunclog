1281f26d719149e901d9f2a2902253f6668f4b37
nodeunclog <nodeunclog@gmail.com>
2015-11-24 03:21:51 +0530

    5.1.4 smallilies 3.3, gulpfile, sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
-const gulp = require('gulp');
+require('unclog')('p');
+process.title = cwd.split(/[\/\\]+/g).slice(2).reverse().join(' ') + ' - Gulp';
 const del = require('del');
+const gulp = require('gulp');
+const gutil = require('gulp-util');
 const babel = require('gulp-babel');
 const replace = require('gulp-replace');
 const changed = require('gulp-changed-in-place');
-
-process.title = 'Gulp: Unclog';
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('clean', done =>
-    del('lib', done));
+    del('lib/**/*', done));
 
 gulp.task('babel', done =>
     gulp.src('src/**/*.js')
     .pipe(changed({firstPass: true}))
+    .pipe(sourcemaps.init())
     .pipe(babel({
         presets: ['es2015', 'stage-0'],
         retainLines: 'true',
     }))
     .pipe(replace(/(\/\/ )?["']use strict['"];?([\n\r]+)?/g, ''))
+    .pipe(sourcemaps.write('.', {sourceRoot: 'src'}))
     .pipe(gulp.dest('lib')));
 
 gulp.task('build', gulp.series('babel'));
